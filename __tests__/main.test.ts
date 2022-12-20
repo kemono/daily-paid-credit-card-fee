3144dc07ee34f8fccedff363486c74d32e566b31
kemono <furry@kemono.app>
2022-12-20 10:36:33 +0900

    test updated

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,5 +3,7 @@ import { dailyPaid } from '../src/main';
 describe('Main test', () => {
     test('dailyPaid', ()=> {
         expect(dailyPaid(3650, 0.1)).toBe(1);
+        expect(dailyPaid(0, 0.1)).toBe(0);
+        expect(dailyPaid(3650, 0)).toBe(0);
     });
 });
