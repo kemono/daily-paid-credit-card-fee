afeb54ca37fdaf759097e150139eb60c02b4ae79
kemono <furry@kemono.app>
2022-12-20 10:55:50 +0900

    コードを修正中

--- src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.dailyPaid = void 0;
-function dailyPaid(indebteedness, rate) {
-    return (indebteedness * rate) / 365;
-}
-exports.dailyPaid = dailyPaid;
-console.log(dailyPaid(3650, 0.1));
